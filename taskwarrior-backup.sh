524c3ba0b90959e289389968abd1421e6a2e6d94
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2015-02-14 15:10:12 +0200

    Store backups in .

--- taskwarrior-backup.sh
+++ taskwarrior-backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Create an archive of the Taskwarrior $TASK_DIR
-cd $HOME/Dropbox/Backup/Taskwarrior && tar -czf task-backup-$(date +'%Y%m%d%H%M%S').tar.gz -C $TASK_DIR .
+# Backups are kept in $TASK_BACKUP_DIR
+cd $TASK_BACKUP_DIR && tar -czf task-backup-$(date +'%Y%m%d%H%M%S').tar.gz -C $TASK_DIR .
